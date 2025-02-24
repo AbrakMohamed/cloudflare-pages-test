4f802b4618edf8eec1dc37ea40f1d0cd996baff3
Mohamed abrak <mabrak.externe@portnet.ma>
2025-02-24 15:44:46 +0000

    9 commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>CAPTCHA Token: {token}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
